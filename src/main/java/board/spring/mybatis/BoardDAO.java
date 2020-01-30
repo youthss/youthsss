ca4cf792a60c192b6bc4acc07eadc4d9d9ffcbc4
youthss <youthss@hanmail.net>
2020-01-30 11:42:45 +0900

    Update BoardDAO.java

--- src/main/java/board/spring/mybatis/BoardDAO.java
+++ src/main/java/board/spring/mybatis/BoardDAO.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+////////정현의
 public class BoardDAO {
 	@Autowired
 	SqlSession session;
