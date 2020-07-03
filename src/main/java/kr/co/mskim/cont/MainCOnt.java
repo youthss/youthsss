bc4f48a2729c10939e3504b0c24b785dd6172f6d
MSKim <mslovehacker@gmail.com>
2020-07-03 14:27:18 +0900

    asdfasdf

---
+++ src/main/java/kr/co/mskim/cont/MainCOnt.java
@@ -0,0 +1,12 @@
+package kr.co.mskim.cont;
+
+public class MainCOnt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("TestMS");
+
+	}
+
+}
